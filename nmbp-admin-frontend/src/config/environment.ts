e6723f127e87b7775ec0804bb2b59ec53208c5b6
pillirs-spec <pillirs@dewsolutions.in>
2026-02-27 16:19:56 +0530

    loader skip

--- nmbp-admin-frontend/src/config/environment.ts
+++ nmbp-admin-frontend/src/config/environment.ts
@@ -1,16 +1,21 @@
 export const environment = {
-    production: process.env.NODE_ENV === 'production',
-    useLocalStorage: process.env.REACT_APP_USE_LOCAL_STORAGE === 'true',
-    appPreferencesPrefix: process.env.REACT_APP_PREFERENCES_PREFIX || 'ts:admin:',
-    authApiBaseUrl: process.env.REACT_APP_AUTH_API_BASE_URL || 'http://localhost:7001',
-    userApiBaseUrl: process.env.REACT_APP_USER_API_BASE_URL || 'http://localhost:7002',
-    adminApiBaseUrl: process.env.REACT_APP_ADMIN_API_BASE_URL || 'http://localhost:7003',
-    devoteeApiUrl: process.env.REACT_APP_DEVOTEE_API_BASE_URL || 'http://localhost:7004',
-    encDecSecretKey: process.env.REACT_APP_ENC_DEC_SECRET_KEY || 'TS@$#&*(!@%^&',
-    defaultDevoteeTrialPeriod: process.env.REACT_APP_DEFAULT_DEVOTEE_TRIAL_PERIOD || 3,
-    skipLoaderRoutes: [
-        '/api/v1/auth/health'
-    ],
-    footerHiddenRoutes : ['/login', '/reset-password', '/forget-password'],
+  production: process.env.NODE_ENV === "production",
+  useLocalStorage: process.env.REACT_APP_USE_LOCAL_STORAGE === "true",
+  appPreferencesPrefix: process.env.REACT_APP_PREFERENCES_PREFIX || "ts:admin:",
+  authApiBaseUrl:
+    process.env.REACT_APP_AUTH_API_BASE_URL || "http://localhost:7001",
+  userApiBaseUrl:
+    process.env.REACT_APP_USER_API_BASE_URL || "http://localhost:7002",
+  adminApiBaseUrl:
+    process.env.REACT_APP_ADMIN_API_BASE_URL || "http://localhost:7003",
+  devoteeApiUrl:
+    process.env.REACT_APP_DEVOTEE_API_BASE_URL || "http://localhost:7004",
+  encDecSecretKey: process.env.REACT_APP_ENC_DEC_SECRET_KEY || "TS@$#&*(!@%^&",
+  defaultDevoteeTrialPeriod:
+    process.env.REACT_APP_DEFAULT_DEVOTEE_TRIAL_PERIOD || 3,
+  skipLoaderRoutes: [
+    "/api/v1/auth/health",
+    "/api/v1/admin/locations/activities",
+  ],
+  footerHiddenRoutes: ["/login", "/reset-password", "/forget-password"],
 };
-  
