3127d1bd8623e676848c4a78a6550a95e443bffd
pillirs-spec <pillirs@dewsolutions.in>
2026-02-28 10:52:34 +0530

    user frontend removed

--- nmbp-user-frontend/src/contexts/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./LoaderContext";
-export * from "./ToastContext";
-export * from "./AuthContext";
-export * from "./LoggerContext";
--- nmbp-user-frontend/src/enums/MenuAccess.ts
+++
@@ -1,4 +0,0 @@
-export enum MenuAccess {
-    READ = "Read",
-    WRITE = "Write"
-}
